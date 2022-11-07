1302a0645d92cb89e32386e39a2247bf211d08fa
Cauã Belodi <115277406+Cauabc@users.noreply.github.com>
2022-11-07 07:02:06 -0300

    To-do list with javascript

--- Lista de Tarefas/script.js
+++ Lista de Tarefas/script.js
@@ -1,17 +1,53 @@
-let input = document.querySelector('#tarefa')
-let btn = document.querySelector('#botao')
-let lista = document.getElementsByTagName('select')[0]
-let contem = []
+const btn = document.querySelector('#add-btn')
 
 
-btn.addEventListener('click', () => {
-    if (input.value.length == 0 || contem.includes(input.value)){
-        return alert("Error")
-    }
-    let item = document.createElement('option')
-    let texto = document.createTextNode(input.value)
-    lista.appendChild(item)
-    item.appendChild(texto)
-    contem.push(input.value)
-    console.log(contem)
+function addTask(){
+    // titulo da tarefa
+    const taskTitle = document.querySelector('#task-title').value
+    
+    if (taskTitle){
+        const template = document.querySelector('.template')
+        //clonar o template 
+        const newTask = template.cloneNode(true)
+        
+        //adicionar titulo
+        newTask.querySelector('.task-title').textContent = taskTitle
+
+        //remover as classes
+        newTask.classList.remove("template")
+        newTask.classList.remove("hide")
+
+        //adicionar tarefa na lista
+        let lista = document.querySelector('#task-list')
+        lista.appendChild(newTask)
+        
+        //limpar texto
+        document.querySelector('#task-title').value = ''
+
+        // adicionar evento de remover
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener('click', function(){
+            removeTask(this)
+        })
+
+        //adicioanr evento de done
+        const doneBtn = newTask.querySelector(".done-btn").addEventListener('click', function(){
+            doneTask(this)
+        })
+    } 
+    
+}
+
+
+function removeTask(task){
+    task.parentNode.remove(true)
+}
+
+function doneTask(task){
+    task.parentNode.classList.toggle('done')
+}
+
+btn.addEventListener('click', function(event){
+
+    event.preventDefault() //para evitar que o formulario seja enviado, do btn type submit
+    addTask()
 })
